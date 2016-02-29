d42bf03c28433dd91461d1c45825d1b3541ff718
Frank Wang <frank@xpliant.com>
2016-02-28 23:31:13 -0800

    add new FIFO design sd_fifo_tailwr

---
+++ env/verilog/fifo_c/bench_fifo_tailwr.v
@@ -0,0 +1,143 @@
+`timescale 1ns/1ns
+
+module bench_fifo_c;
+
+  reg clk, reset;
+
+  localparam width = 8;
+  localparam depth = 7;
+  localparam usz = $clog2(depth+1);
+
+  initial clk = 0;
+  always #10 clk = ~clk;
+
+  /*AUTOWIRE*/
+  // Beginning of automatic wires (for undeclared instantiated-module outputs)
+  logic [7:0]           chk_data;               // From fifo_s of sd_fifo_tailwr.v
+  wire                  chk_drdy;               // From chk of sd_seq_check.v
+  logic                 chk_srdy;               // From fifo_s of sd_fifo_tailwr.v
+  wire [width-1:0]      gen_data;               // From gen of sd_seq_gen.v
+  logic                 gen_drdy;               // From fifo_s of sd_fifo_tailwr.v
+  wire                  gen_srdy;               // From gen of sd_seq_gen.v
+  logic [usz-1:0]       nxt_usage;              // From fifo_s of sd_fifo_tailwr.v
+  logic [usz-1:0]       usage;                  // From fifo_s of sd_fifo_tailwr.v
+  // End of automatics
+
+/* sd_seq_gen AUTO_TEMPLATE
+ (
+ .p_\(.*\)   (gen_\1[]),
+ );
+ */
+  sd_seq_gen gen
+    (/*AUTOINST*/
+     // Outputs
+     .p_srdy                            (gen_srdy),              // Templated
+     .p_data                            (gen_data[width-1:0]),   // Templated
+     // Inputs
+     .clk                               (clk),
+     .reset                             (reset),
+     .p_drdy                            (gen_drdy));             // Templated
+
+/* sd_seq_check AUTO_TEMPLATE
+ (
+ .c_\(.*\)   (chk_\1[]),
+ );
+ */
+  sd_seq_check #(.width(width)) chk
+    (/*AUTOINST*/
+     // Outputs
+     .c_drdy                            (chk_drdy),              // Templated
+     // Inputs
+     .clk                               (clk),
+     .reset                             (reset),
+     .c_srdy                            (chk_srdy),              // Templated
+     .c_data                            (chk_data[width-1:0]));  // Templated
+
+/* sd_fifo_tailwr AUTO_TEMPLATE
+ (
+     .p_\(.*\)   (chk_\1[]),
+     .c_\(.*\)   (gen_\1[]),
+ );
+ */
+  sd_fifo_tailwr #(.width(8), .depth(depth)) fifo_s
+    (/*AUTOINST*/
+     // Outputs
+     .c_drdy                            (gen_drdy),              // Templated
+     .p_data                            (chk_data[7:0]),         // Templated
+     .p_srdy                            (chk_srdy),              // Templated
+     .nxt_usage                         (nxt_usage[usz-1:0]),
+     .usage                             (usage[usz-1:0]),
+     // Inputs
+     .clk                               (clk),
+     .reset                             (reset),
+     .c_data                            (gen_data[7:0]),         // Templated
+     .c_srdy                            (gen_srdy),              // Templated
+     .p_drdy                            (chk_drdy));             // Templated
+
+  initial
+    begin
+`ifdef MODEL_TECH
+      $wlfdumpvars(0, bench_fifo_c);
+`else
+      //$dumpfile("fifo_s.vcd");
+      //$dumpvars;
+      $vcdpluson();
+`endif
+      reset = 1;
+      #100;
+      reset = 0;
+
+      gen.rep_count = 9000;
+
+      // burst normal data for 20 cycles
+      repeat (20) @(posedge clk);
+
+      gen.srdy_pat = 8'h5A;
+      repeat (20) @(posedge clk);
+
+      chk.drdy_pat = 8'hA5;
+      repeat (40) @(posedge clk);
+
+      // check FIFO overflow
+      gen.srdy_pat = 8'hFD;
+      chk.drdy_pat = 8'h03;
+      repeat (100) @(posedge clk);
+
+      // check FIFO underflow
+      gen.srdy_pat = 8'h11;
+      chk.drdy_pat = 8'hEE;
+      repeat (100) @(posedge clk);
+
+      // Run out the remainder of the repeat count
+      fork
+        begin : runout
+          while (gen.rep_count > 0) 
+            begin
+              gen.srdy_pat = {$random} | (1 << ($random % 8));
+              chk.drdy_pat = {$random} | (1 << ($random % 8));
+              repeat (16) @(posedge clk);
+            end
+        end
+        begin : timeout
+          repeat (10000) @(posedge clk);
+          disable runout;
+        end
+      join
+
+      if (chk.ok_cnt >= 1000)
+        $display ("----- TEST PASSED -----");
+      else
+        begin
+          $display ("***** TEST FAILED *****");
+          $display ("Ok count=%4d", chk.ok_cnt);
+        end
+
+
+      #5000;
+      $finish;
+    end
+
+endmodule // bench_fifo_s
+// Local Variables:
+// verilog-library-directories:("." "../common" "../../../rtl/verilog/buffers")
+// End:
---
+++ rtl/verilog/buffers/sd_fifo_tailwr.v
@@ -0,0 +1,138 @@
+//----------------------------------------------------------------------
+// Author: Frank Wang
+// 
+// variant of sd_fifo_c, always write to last location and then shift
+//
+//----------------------------------------------------------------------
+`ifndef _SD_FIFO_TAILWR_V_
+`define _SD_FIFO_TAILWR_V_
+module sd_fifo_tailwr #(
+    parameter width=8,
+    parameter rst_sz=0, //upper bits of data to reset if reset is asserted
+    parameter depth=16,
+    parameter usz=$clog2(depth+1)
+) (
+    input   clk,
+    input   reset,
+
+    input   logic [width-1:0] c_data,
+    input   logic             c_srdy,
+    output  logic             c_drdy,
+
+    output  logic [width-1:0] p_data,
+    output  logic             p_srdy,
+    input   logic             p_drdy,
+
+    output  logic [usz-1:0]   nxt_usage,
+    output  logic [usz-1:0]   usage
+);
+
+localparam asz=$clog2(depth);
+logic [depth-1:0][width-1:0]  data_buf;
+logic [depth-1:0][width-1:0]  nxt_data_buf;
+logic rd_vld;
+logic wr_vld;
+logic wr_vld_d;
+logic rd_vld_d;
+logic [usz-1:0]   partial_usage;
+logic [usz-1:0]   nxt_partial_usage;
+logic [usz-1:0]   nxt_complete_usage;
+logic buf_shift;
+logic buf_wr_en;
+logic [asz-1:0] buf_rd_ptr;
+logic [usz-1:0] usage_d;
+logic nxt_buf_shift;
+logic nxt_buf_wr_en;
+logic [asz-1:0] nxt_buf_rd_ptr;
+
+assign rd_vld = p_srdy && p_drdy;
+assign wr_vld = c_srdy && c_drdy;
+  
+assign c_drdy = (usage < depth);
+//always @(posedge clk) begin
+//    if (reset)                          c_drdy <= 1'b0;
+//    else if(usage < depth)              c_drdy <= 1'b1;
+//    else if((usage == depth) && p_drdy) c_drdy <= 1'b1;
+//    else                                c_drdy <= 1'b0;
+//end
+// read from fifo
+assign p_srdy = (usage > 0);
+assign p_data = data_buf[buf_rd_ptr];
+
+// writing and shifting of FIFO
+always @(*) begin
+    nxt_data_buf = data_buf;
+    if(nxt_buf_shift) begin  // shifting
+        for(int i=0; i<depth-1; i=i+1) begin
+            nxt_data_buf[i] = data_buf[i+1];
+        end
+    end
+    if(nxt_buf_wr_en) begin
+        nxt_data_buf[depth-1] = c_data;
+    end
+end
+always @(posedge clk) begin
+    data_buf <= nxt_data_buf;
+//    if(rst_sz>0)
+//        if(reset) data_buf[depth-1][width-1 -: rst_sz] <= {rst_sz{1'b0}};
+end
+
+// control
+// number two of total two fan-out for c_srdy
+assign nxt_complete_usage  = usage         + wr_vld   - rd_vld;
+assign nxt_partial_usage   = partial_usage + wr_vld_d - rd_vld;
+assign usage = wr_vld_d + partial_usage;
+assign nxt_usage = nxt_complete_usage;
+always @(posedge clk) begin
+    if(reset) begin
+        wr_vld_d <= 1'b0;
+        partial_usage    <= {usz{1'b0}};
+        buf_shift   <= 1'b0;
+        buf_wr_en   <= 1'b0;
+        buf_rd_ptr  <= depth[asz-1:0]-1'b1;
+        //usage_d     <= {usz{1'b0}};
+    end else begin
+        wr_vld_d <= wr_vld;
+        //rd_vld_d <= p_srdy && p_drdy;
+        partial_usage   <= nxt_partial_usage;
+        buf_shift   <= nxt_buf_shift;
+        buf_wr_en   <= nxt_buf_wr_en;
+        buf_rd_ptr  <= nxt_buf_rd_ptr;
+        //usage_d     <= usage;
+    end
+end
+always @(*) begin
+    nxt_buf_shift = ((usage < depth) && wr_vld_d) || ((usage == depth) && rd_vld);
+    nxt_buf_wr_en = (usage < depth) || (p_srdy && p_drdy);
+    nxt_buf_rd_ptr = ( nxt_buf_shift &&  (p_srdy && p_drdy)) ? buf_rd_ptr      :
+                     ( nxt_buf_shift && ~(p_srdy && p_drdy)) ? buf_rd_ptr-1'b1 :
+                     (~nxt_buf_shift &&  (p_srdy && p_drdy)) ? buf_rd_ptr+1'b1 : buf_rd_ptr;
+end
+
+
+`ifdef SD_INLINE_ASSERTION_ON
+logic [usz-1:0]   complete_usage;
+always @(posedge clk) begin
+    if(reset) begin
+        complete_usage   <= {usz{1'b0}};
+    end else begin
+        complete_usage   <= nxt_complete_usage;
+    end
+end
+
+logic fifo_full;
+logic chk_usage;
+
+assign fifo_full = (usage >= depth);
+assign chk_usage = (complete_usage == usage);
+
+COVER_FIFO_FULL: cover property 
+        (@(posedge clk) disable iff (reset) fifo_full);
+ERROR_FIFO_USAGE: assert property 
+        (@(posedge clk) disable iff (reset) (complete_usage == usage));
+`endif
+
+endmodule // sd_fifo_tailwr
+// Local Variables:
+// End:
+`endif //  _SD_FIFO_TAILWR_V_
