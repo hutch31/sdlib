605bc47771b1f8e5112d4139dfede3a3d053246f
Guy Hutchison <ghutchis@gmail.com>
2016-04-10 21:02:03 -0700

    Added Chisel translations of Input, Output and Mirror
    Translations are based on DecoupledIO type, which maps to srdy/drdy
    as:
      srdy = valid
      drdy = ready
      data = bits

---
+++ rtl/chisel/DC.scala
@@ -0,0 +1,76 @@
+package DCUtil
+
+import Chisel._
+
+class DCInput[T <: Data](data: T) extends Module {
+  val io = new Bundle {
+    val c = new DecoupledIO(data).flip
+    val p = new DecoupledIO(data)
+  }
+  val occ = Reg(init = Bool(false))
+  val nxt_occ = io.c.valid & !io.p.ready | !io.p.ready & occ
+  occ := nxt_occ
+  val hold = Reg(io.c.bits)
+  val load = io.c.valid & !io.p.ready & !occ | io.c.valid & io.p.ready & occ
+  when (load) { hold := io.c.bits }
+  io.p.bits := Mux(occ, hold, io.c.bits)
+  io.p.valid := io.c.valid | occ
+  io.c.ready := io.p.ready | !occ
+}
+
+class DCOutput[T <: Data](data: T) extends Module {
+  val io = new Bundle {
+    val c = new DecoupledIO(data).flip
+    val p = new DecoupledIO(data)
+  }
+  val nxt_p_valid = io.c.valid | (!io.p.ready & io.p.valid)
+  io.p.valid := Reg(next = nxt_p_valid, init=Bool(false))
+  io.c.ready := io.p.ready | !io.p.valid
+  val load = io.c.valid & io.c.ready
+  val nxt_p_data = UInt(io.c.bits)
+  io.p.bits := Reg(next = nxt_p_data)
+}
+
+class DCMirror[T <: Data](data: T, mirror: Int) extends Module {
+  val io = new Bundle {
+    val c = new DecoupledIO(data).flip
+    val c_dst = UInt(INPUT, width = mirror)
+    val p = Vec(mirror, new DecoupledIO(data))
+  }
+  // create a single holding register and tie all output data signals to it
+  val hold = Reg(UInt(data))
+  for (i <- 0 to mirror-1 by 1)
+    io.p(i).bits := hold
+  // create internal buses to represent collective ready and valid signals
+  val p_drdy = UInt(width = mirror)
+  for (i <- 0 to mirror-1 by 1)
+    p_drdy(i) := io.p(i).ready
+  val nxt_p_srdy = UInt(width = mirror)
+  val p_srdy = Reg(next = nxt_p_srdy, init = UInt(0))
+  val nxt_accept = (p_srdy === UInt(0)) | ((p_srdy != UInt(0)) & ((p_srdy & p_drdy) === p_srdy))
+  io.c.ready := nxt_accept
+  nxt_p_srdy := Mux(nxt_accept, Fill(mirror, io.c.valid) & io.c_dst, p_srdy & ~p_drdy)
+  when (io.c.valid & nxt_accept) {
+    hold := io.c.bits
+  }
+}
+
+class WrapInput(w: Int) extends Module {
+  val io = new Bundle {
+    val c = new DecoupledIO(UInt(width=w)).flip
+    val p = new DecoupledIO(UInt(width=w))
+  }
+  val dci = Module(new DCInput(io.c.bits))
+  val dco = Module(new DCOutput(io.c.bits))
+  io.c <> dci.io.c
+  dci.io.p <> dco.io.c
+  dco.io.p <> io.p
+}
+
+object mainObject {
+  def main(args: Array[String]): Unit = {
+    //val tutArgs = args.slice(1, args.length)
+    val tutArgs = Array("--backend", "v")
+    chiselMain(tutArgs, () => Module(new WrapInput(32)))
+  }
+}
---
+++ rtl/chisel/build.sbt
@@ -0,0 +1,4 @@
+scalaVersion := "2.11.7"
+version := "2.3-SNAPSHOT"
+libraryDependencies += "edu.berkeley.cs" %% "chisel" % "latest.release"
+
