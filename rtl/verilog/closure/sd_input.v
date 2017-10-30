1e8251c7571b54052b53e1d91b1556bbfb9d1406
Guy Hutchison <ghutchison@cavium.com>
2017-10-30 16:38:56 -0700

    Added/reverted file headers to unlicense

--- rtl/verilog/closure/sd_input.v
+++ rtl/verilog/closure/sd_input.v
@@ -1,11 +1,42 @@
 //----------------------------------------------------------------------
-// Author: Heeloo Chung
-// Xpliant Inc. proprietary and confidential
+// Srdy/Drdy high-fanout iofull block
 //
-// implementation base on sd_input. This version has no reset for the
-// datapath (hold signal).
+// High-fanout version of sd_iofull.  Instantiates multiple sd_output
+// blocks in parallel.  Block is the number of bits going to each
+// sd_output block.  Width is total width; it does not need to be
+// a multiple of block size, but must meet:
+//    width > (ctlfan-1)*block+1
 //
+// Naming convention: c = consumer, p = producer, i = internal interface
 //----------------------------------------------------------------------
+// Author: Guy Hutchison
+//
+// This is free and unencumbered software released into the public domain.
+//
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
+//
+// In jurisdictions that recognize copyright laws, the author or authors
+// of this software dedicate any and all copyright interest in the
+// software to the public domain. We make this dedication for the benefit
+// of the public at large and to the detriment of our heirs and
+// successors. We intend this dedication to be an overt act of
+// relinquishment in perpetuity of all present and future rights to this
+// software under copyright law.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+//
+// For more information, please refer to <http://unlicense.org/> 
+//----------------------------------------------------------------------
+
 `ifndef _SD_INPUT_V_
 `define _SD_INPUT_V_
 
