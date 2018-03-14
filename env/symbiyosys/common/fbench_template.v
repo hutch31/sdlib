0940dd799eb43e16865e312db4a377897df2db57
Guy Hutchison <ghutchison@cavium.com>
2018-03-14 16:53:28 -0700

    Added regression script and regression for common modules

---
+++ env/symbiyosys/common/fbench_template.v
@@ -0,0 +1,47 @@
+module fbench_$module
+ #(parameter width = 16)
+ (
+  input clk,
+
+  input [width-1:0]   c_data,
+  input               c_srdy,
+  output              c_drdy,
+  output [width-1:0]  p_data,
+  output              p_srdy,
+  input               p_drdy
+ );
+
+  logic reset = 1;
+  wire [31:0] in_count, out_count;
+
+  initial
+    begin
+      assume(reset);
+    end
+
+  $module #(.width(width)) dut
+    (.clk    (clk), 
+     .reset  (reset),
+     .${cons}_srdy (c_srdy),
+     .${cons}_drdy (c_drdy),
+     .${cons}_data (c_data),
+     .${prod}_srdy (p_srdy),
+     .${prod}_drdy (p_drdy),
+     .${prod}_data (p_data)
+    );
+
+  sd_common_checks #(.width(width)) common
+    (.clk    (clk), 
+     .reset  (reset),
+     .c_srdy (c_srdy),
+     .c_drdy (c_drdy),
+     .c_data (c_data),
+     .p_srdy (p_srdy),
+     .p_drdy (p_drdy),
+     .p_data (p_data),
+
+     .in_count (in_count),
+     .out_count (out_count));
+
+endmodule
+
---
+++ env/symbiyosys/regression.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import unittest
+import subprocess
+import os
+import string
+
+def find_file(filename):
+    for root, dirs, files in os.walk("../../.."):
+        for file in files:
+            if file == filename:
+                filepath = os.path.join(root, file)
+                print (filepath)
+                return filepath
+
+def create_template(module, cons="c", prod="p"):
+    with open("fbench_template.v", "r") as fh:
+        data = fh.read()
+    template = string.Template(data)
+    result = template.substitute(module=module, prod=prod, cons=cons)
+    with open("fbench_"+module+".v", "w") as fh:
+        fh.write(result)
+
+    modulepath = find_file(module+".v")
+
+    with open("template.sby", "r") as fh:
+        data = fh.read()
+    template = string.Template(data)
+    result = template.substitute(module=module, modulepath=modulepath)
+    with open(module+".sby", "w") as fh:
+        fh.write(result)
+    
+class SymTests(unittest.TestCase):
+    def test_common(self):
+        os.chdir("common")
+        for e, m in enumerate([["sd_input", "c", "ip"], 
+                               ["sd_output", "ic", "p"],
+                               ["sd_iohalf", "c", "p"],
+                               ["sd_iofull", "c", "p"]]):
+            with self.subTest(i=e):
+                create_template(m[0], cons=m[1], prod=m[2])
+                output = subprocess.check_output(["sby", "-f", m[0] + ".sby"])
+                self.assertTrue(output.find("PASS") != -1)
+
+    @unittest.skip
+    def test_fifo_c(self):
+        os.chdir("fifo_c")
+        output = subprocess.check_output(["sby", "-f", "fifo_c.sby"])
+        self.assertTrue(output.find("PASS") != -1)
+
+if __name__ == '__main__':
+    unittest.main()
