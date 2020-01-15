319114ee25de8e1aba537613718a9a1527799d7b
Gitea <gitea@fake.local>
2020-01-14 16:45:32 -0800

    Added test runner for use by actions

---
+++ scripts/run_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+
+parser = argparse.ArgumentParser(description='Run an sdlib test and check results')
+parser.add_argument('--test', help="Module name for test")
+
+args = parser.parse_args()
+
+os.chdir('env/verilog/'+args.test)
+os.system("iverilog -g2012 -f {}.vf".format(args.test))
+os.system("./a.out > test.log")
+
+passed = False
+with open("test.log", "r") as fh:
+    for fd in fh:
+        if fd.find("TEST PASSED") != -1:
+            passed = True
+
+if passed:
+    sys.exit(0)
+else:
+    sys.exit(1)
